f967492ba2e3bee1b7402084fd392f96823eb6ff
JDamian-Mendoza <jmdamian.mendoza@gmail.com>
2017-02-27 19:23:48 -0500

    parte1
    primera parte alquiler

---
+++ HOTEL_SUSAN/HOTEL_SUSAN/frmHabitacion_Alquiler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HOTEL_SUSAN
+{
+    public partial class frmHabitacion_Alquiler : Form
+    {
+        public frmHabitacion_Alquiler()
+        {
+            InitializeComponent();
+        }
+
+        private void frmHabitacion_Alquiler_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
